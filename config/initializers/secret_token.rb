698b3118f043015df7cbe9af94fb785696526038
Sami Makhuli <smakhuli@yahoo.com>
2012-06-25 22:33:30 -0700

    'firstcommit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bethel2::Application.config.secret_token = 'bf80245d6bbdcf688310d09af688a9d5145d1b4297f49eb62293d94a8b39a1f1ee41ca786687e3312f76cd4be9b1158ac9692995686df211f53ce85864f7ed8d'
