828e7d00ffbb9b59cc57256a8a0f7449e5971a91
Sami Makhuli <smakhuli@yahoo.com>
2012-06-26 12:40:35 -0700

    'firstcommit062612'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.delete_all
+
+Student.create :name => "John Smith", :year => 1, :sex => "Male", :country => "USA", :tuition_paid => 1000, :tuition_due => 3200
+Student.create :name => "Anne Johnson", :year => 3, :sex => "Female", :country => "Sweden", :tuition_paid => 3200, :tuition_due => 0
+Student.create :name => "Sally Jones", :year => 2, :sex => "female", :country => "Ireland", :tuition_pad => 2400, :tuition_due => 800
